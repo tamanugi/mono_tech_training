770655f3dbc94c499bf9e4f4d29a742a6580109e
tamanugi <tamanugi@gmail.com>
2021-05-30 13:44:37 +0900

    implment user search page HTML

--- mono-front/src/views/Home.vue
+++ mono-front/src/views/Home.vue
@@ -1,8 +1,34 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+<div>
+  <h3>検索条件</h3>
+  <div>
+    <label>User Name</label><input type="text">
+    <button>Search</button>
   </div>
+  <h3>検索結果</h3>
+  <table border="1">
+    <thead>
+      <tr>
+        <th>User Id</th>
+        <th>User Namme</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><a href="detail.html">001</a></td>
+        <td>Tanaka</td>
+      </tr>
+      <tr>
+        <td><a href="detail.html">002</a></td>
+        <td>Tamura</td>
+      </tr>
+      <tr>
+        <td><a href="detail.html">003</a></td>
+        <td>Nakata</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 </template>
 
 <script lang="ts">
