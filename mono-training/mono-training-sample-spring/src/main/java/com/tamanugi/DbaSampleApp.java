3ea6e7803bd97dbedda99e9108814bfb746d2d02
tamanugi <tamanugi@gmail.com>
2021-06-21 16:47:21 +0900

    create project use sit-dba

---
+++ mono-training/mono-training-sample-spring/src/main/java/com/tamanugi/DbaSampleApp.java
@@ -0,0 +1,8 @@
+package com.tamanugi;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbaSampleApp {
+
+}
---
+++ mono-training/mono-training-sample-spring/src/main/java/com/tamanugi/domain/persion/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.tamanugi.domain.persion;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends CrudRepository<PersonEntity, Integer> {
+
+  List<PersonEntity> findByName(String name);
+}
