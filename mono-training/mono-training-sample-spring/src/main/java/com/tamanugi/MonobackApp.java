7ab48abfe8f7de098b4bb58347b673eea45491d9
tamanugi <tamanugi@gmail.com>
2021-06-22 16:12:35 +0900

    renam Backend entrypoint class

---
+++ mono-training/mono-training-sample-spring/src/main/java/com/tamanugi/MonobackApp.java
@@ -0,0 +1,13 @@
+package com.tamanugi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonobackApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonobackApp.class, args);
+    }
+
+}
